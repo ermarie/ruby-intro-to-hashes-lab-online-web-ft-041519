146159f599b8ab33b747dea512361f5aad554913
Erin Stalzer <erinstalzer@gmail.com>
2019-04-19 14:20:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,9 +17,9 @@ def monopoly_with_second_tier
   monopoly = {}
   monopoly[:railroads] = {}
   monopoly[:railroads] = {
-    :pieces = 4,
-    :names = {},
-    :rent_in_dollars = {}
+    :pieces => 4,
+    :names => {},
+    :rent_in_dollars => {}
   }
 end
 
